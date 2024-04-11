f1b61f11b0347de234c8d9452c32fe3b25319cb2
giuliapetrilli <236888@students.hertie-school.org>
2024-04-11 17:29:52 +0200

    getting sensor data throgh API

---
+++ Sonitus_API.py
@@ -0,0 +1,5 @@
+# get sensor data from https://data.smartdublin.ie/sonitus-api
+# map that displays the data : https://dublincityairandnoise.ie/
+# page : https://data.gov.ie/dataset/sonitus/resource/38a117c9-79b5-4e1c-9080-ed862bbe689d
+
+
